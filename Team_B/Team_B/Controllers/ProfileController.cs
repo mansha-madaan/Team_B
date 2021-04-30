7114641c26f355ff9622cea14e2a3b67bb8144e4
Mansha Madaan <mansha.madaan@cygrp.com>
2021-04-30 17:06:27 +0530

    Implemented profile controller

---
+++ Team_B/Team_B/Controllers/ProfileController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Team_B.DbModels;
+
+namespace Team_B.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private EmployeeDBContext _context;
+
+        public ProfileController(EmployeeDBContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Profile
+        [HttpGet]
+        public ActionResult GetAllEmp()
+        {
+            var emplist = _context.ProfileData.ToList();
+            return Ok(emplist);
+        }
+
+        // GET: api/Profile/5
+        [HttpGet("{id}")]
+        public ActionResult GetUserById(int id)
+        {
+            var emp = _context.ProfileData.Where(x => x.EmpId == id);
+            return Ok(emp);
+        }
+
+
+        // POST: api/Profile
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Profile/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Team_B/Team_B/DbModels/ProfileData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_B.DbModels
+{
+    public class ProfileData
+    {
+        public int? EmpId { get; set; }
+        public int Pid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string Location { get; set; }
+        public string Role { get; set; }
+        public string TotalExp { get; set; }
+        public DateTime? DateJoin { get; set; }
+        public string Skills { get; set; }
+
+        public EmpLogin Emp { get; set; }
+    }
+}
