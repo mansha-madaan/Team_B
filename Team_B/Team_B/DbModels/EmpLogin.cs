03ff4b49d0ff527b69b6d82ae155dc42f3d89a80
Mansha Madaan <mansha.madaan@cygrp.com>
2021-05-01 11:30:10 +0530

    Updated

--- Team_B/Team_B/DbModels/EmpLogin.cs
+++ Team_B/Team_B/DbModels/EmpLogin.cs
@@ -8,5 +8,7 @@ namespace Team_B.DbModels
         public int EmpId { get; set; }
         public string EmpEmailId { get; set; }
         public string EmpPassword { get; set; }
+
+        public ICollection<ProfileData> ProfileData { get; set; }
     }
 }
--- Team_B/Team_B/DbModels/EmployeeDBContext.cs
+++ Team_B/Team_B/DbModels/EmployeeDBContext.cs
@@ -16,13 +16,14 @@ namespace Team_B.DbModels
         }
 
         public virtual DbSet<EmpLogin> EmpLogin { get; set; }
+        public virtual DbSet<ProfileData> ProfileData { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=CYG362;Database=EmployeeDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=CYG389;Database=EmployeeDB;Trusted_Connection=True;");
             }
         }
 
@@ -41,6 +42,53 @@ namespace Team_B.DbModels
                 entity.Property(e => e.EmpPassword)
                     .IsRequired()
                     .HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<ProfileData>(entity =>
+            {
+                entity.HasKey(e => e.Pid);
+
+                entity.ToTable("profileData");
+
+                entity.Property(e => e.Pid).HasColumnName("PId");
+
+                entity.Property(e => e.DateJoin)
+                    .HasColumnName("Date_Join")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.EmailId)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Location)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Role)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Skills)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TotalExp)
+                    .HasColumnName("Total_Exp")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Emp)
+                    .WithMany(p => p.ProfileData)
+                    .HasForeignKey(d => d.EmpId)
+                    .HasConstraintName("FK__profileDa__EmpId__5CD6CB2B");
             });
         }
     }
