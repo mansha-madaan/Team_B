5fa0c23fd127e2aebcdaff7fd466d535a036462c
Avneet Kaur <avneet.kaur@cybergindia.local>
2021-05-03 16:16:53 +0530

    updated selfReview

--- reviewPage#4/myReview.js
+++ reviewPage#4/myReview.js
@@ -1,6 +1,61 @@
-function showhide()
+function showhide1()
+{  
+     var div = document.getElementById("effectiveness");  
+     if (div.style.display !== "none") 
+     {  
+         div.style.display = "none";  
+     }  
+     else
+     {  
+         div.style.display = "block";  
+     }  
+}  
+
+function showhide2()
 {  
      var div = document.getElementById("delivery");  
+     if (div.style.display !== "none") 
+     {  
+         div.style.display = "none";  
+     }  
+     else
+     {  
+         div.style.display = "block";  
+     }  
+}  
+
+
+function showhide3()
+{  
+     var div = document.getElementById("leadership");  
+     if (div.style.display !== "none") 
+     {  
+         div.style.display = "none";  
+     }  
+     else
+     {  
+         div.style.display = "block";  
+     }  
+}  
+
+
+function showhide4()
+{  
+     var div = document.getElementById("growth");  
+     if (div.style.display !== "none") 
+     {  
+         div.style.display = "none";  
+     }  
+     else
+     {  
+         div.style.display = "block";  
+     }  
+}  
+
+
+function showhide5()
+{  
+     var div = document.getElementById("feedback");  
      if (div.style.display !== "none") 
      {  
          div.style.display = "none";  
