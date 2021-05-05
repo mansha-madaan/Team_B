d8b8dce91f4987d47324515909b0ae2e9c42c89e
Aditya Sharma <82053463+whatisthisaditya@users.noreply.github.com>
2021-05-05 18:22:11 +0530

    final commit

--- rmsUI/admin finalised/admin/employee/myReviewClosed.js
+++ rmsUI/admin finalised/admin/employee/myReviewClosed.js
@@ -122,7 +122,7 @@ let fillDataIntoTable = (data) => {
 };
 //to do add loader before dom content load and make listener after ajax call
 let doWorkAfterDom = () => {
-  myFunction();
+  
   fetch(baseUrl + "/reviewlist/" + params.get("rid"), {
     method: "GET",
     mode: "cors", // no-cors, *cors, same-origin
@@ -473,11 +473,7 @@ function showhide5() {
     div.style.display = "block";
   }
 }
-function myFunction() {
-  myVar = setTimeout(showPage, 500);
-}
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+function goback() {
+  window.history.back();
 }
